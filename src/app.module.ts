12de7f6639b8d58b46f3d0eefab9aed7ac28e163
matiasmdev <main.mmarquez@gmail.com>
2019-02-26 18:32:20 -0300

    Added the filter and interceptor to the app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,9 @@
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +12,16 @@ import { StudentModule } from './student/student.module';
 @Module({
 	imports: [TypeOrmModule.forRoot(), StudentModule],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [
+		AppService,
+		{
+			provide: APP_FILTER,
+			useClass: HttpErrorFilter,
+		},
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: LoggingInterceptor,
+		},
+	],
 })
 export class AppModule {}
