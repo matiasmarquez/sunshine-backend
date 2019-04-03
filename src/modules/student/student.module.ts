ca9de4d77ddc2931b51a8698a61660c3a970d416
matiasmdev <main.mmarquez@gmail.com>
2019-04-03 18:48:24 -0300

    Added export of service to the module.

--- src/modules/student/student.module.ts
+++ src/modules/student/student.module.ts
@@ -9,5 +9,6 @@ import { StudentRepository } from './student.repository';
 @Module({
 	imports: [TypeOrmModule.forFeature([Student, StudentRepository])],
 	providers: [StudentService, StudentResolver],
+	exports: [StudentService],
 })
 export class StudentModule {}
