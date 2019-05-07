d22925b658fb7aaee5f745cfce78a37264477c38
matiasmdev <main.mmarquez@gmail.com>
2019-05-07 20:10:44 -0300

    Added functions findNotPayed() and countAll().

--- src/modules/inscription/inscription/inscription.repository.ts
+++ src/modules/inscription/inscription/inscription.repository.ts
@@ -17,13 +17,29 @@ export class InscriptionRepository extends Repository<Inscription> {
 		return qb.getMany();
 	}
 
+	public findNotPayed(): Promise<Inscription[]> {
+		const qb = this.qb;
+		this.addJoins(qb);
+		this.addOrder(qb);
+		const date = new Date();
+		const month = date.getMonth() + 1;
+		qb.andWhere('MONTH(installments.date) <= :month', { month });
+		qb.andWhere('installments.paid = :paid', { paid: false });
+		return qb.getMany();
+	}
+
 	public removeInstallments(inscriptionId): Promise<DeleteResult> {
 		const qb = this.qb;
 		return qb
 			.delete()
 			.from(InscriptionInstallment)
-			.where('inscriptionId = :id', { inscriptionId })
+			.where('inscriptionId = :id', { id: inscriptionId })
 			.execute();
+	}
+
+	public countAll(): Promise<Number> {
+		const qb = this.qb;
+		return qb.getCount();
 	}
 
 	private addJoins(
@@ -31,9 +47,7 @@ export class InscriptionRepository extends Repository<Inscription> {
 	): SelectQueryBuilder<Inscription> {
 		qb.leftJoinAndSelect('inscription.course', 'course')
 			.leftJoinAndSelect('inscription.student', 'student')
-			.leftJoinAndSelect('inscription.payStates', 'payStates')
-			.leftJoinAndSelect('inscription.installments', 'installments')
-			.leftJoinAndSelect('payStates.installment', 'payStatesInstallment');
+			.leftJoinAndSelect('inscription.installments', 'installments');
 		return qb;
 	}
 
@@ -42,7 +56,7 @@ export class InscriptionRepository extends Repository<Inscription> {
 	): SelectQueryBuilder<Inscription> {
 		qb.orderBy({
 			'student.name': 'DESC',
-			'installments.number': 'ASC',
+			'installments.date': 'ASC',
 		});
 		return qb;
 	}
