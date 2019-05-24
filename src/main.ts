2ba8e38c4f13d5b7992b9f4a7f20cc200feb72e4
matiasmdev <main.mmarquez@gmail.com>
2019-05-23 22:39:14 -0300

    Added more urls to origins cors.

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,12 @@ const port = process.env.PORT || 8080;
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
 		cors: {
-			origin: 'http://localhost:3000',
+			origin: [
+				'http://localhost:3000',
+				'http://192.168.0.11:3000',
+				'http://www.sunshine.com',
+				'http://sunshine.com',
+			],
 		},
 	});
 	await app.listen(port);
