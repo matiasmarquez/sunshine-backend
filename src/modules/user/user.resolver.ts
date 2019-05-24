deb2c8d0d7580704eab7765b203c32c251b285a2
matiasmdev <main.mmarquez@gmail.com>
2019-05-23 22:37:07 -0300

    Created rest of queries and mutations for users.

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -1,5 +1,6 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UserService } from './user.service';
+import { UserCreateInput, UserUpdateInput } from 'graphql.schema';
 
 @Resolver('User')
 export class UserResolver {
@@ -7,7 +8,27 @@ export class UserResolver {
 
 	@Query()
 	users() {
-		return this.service.showAll();
+		return this.service.find();
+	}
+
+	@Query()
+	user(@Args('id') id: string) {
+		return this.service.findOneById(id);
+	}
+
+	@Mutation()
+	createUser(@Args('data') data: UserCreateInput) {
+		return this.service.create(data);
+	}
+
+	@Mutation()
+	updateUser(@Args('id') id: string, @Args('data') data: UserUpdateInput) {
+		return this.service.update(id, data);
+	}
+
+	@Mutation()
+	deleteUser(@Args('id') id: string) {
+		return this.service.delete(id);
 	}
 
 	private get service(): UserService {
