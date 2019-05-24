314f4bc8232a11efa2c829364b842ae08227022c
matiasmdev <main.mmarquez@gmail.com>
2019-05-23 22:37:24 -0300

    Added crud operations to the service of users.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,46 +1,56 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
 import { User } from './user.entity';
-import { UserDTO, UserRO } from './user.dto';
+
+import { CrudOperations } from 'common/services/crud.service';
+import { UserCreateInput, UserUpdateInput } from 'graphql.schema';
+import { AuthService } from 'modules/auth/auth.service';
 
 @Injectable()
-export class UserService {
+export class UserService extends CrudOperations {
 	constructor(
 		@InjectRepository(User)
 		private userRepository: Repository<User>,
-	) {}
-
-	async showAll(): Promise<UserRO[]> {
-		const users = await this.repository.find();
-		return users.map(user => user.toResponseObject(false));
+	) {
+		super(userRepository);
 	}
 
-	async login(data: UserDTO): Promise<UserRO> {
-		const { username, password } = data;
-		const user = await this.repository.findOne({ username });
-		if (!user || !(await user.comparePassword(password))) {
-			throw new HttpException(
-				'Invalid username or password',
-				HttpStatus.BAD_REQUEST,
-			);
+	find(): Promise<User[]> {
+		return super.findAll();
+	}
+
+	findOneById(id: string): Promise<User> {
+		return super.findOneById(id);
+	}
+
+	async findOneByUsername(username: string): Promise<User> {
+		const user = await this.userRepository.findOne({ username });
+		return user;
+	}
+
+	async create(data: UserCreateInput): Promise<User> {
+		return super.create(data);
+	}
+
+	async update(id: string, data: UserUpdateInput): Promise<User> {
+		let { password, ...rest } = data;
+		if (password === '') {
+			return super.update(id, { ...rest });
 		}
-		return user.toResponseObject();
-	}
-
-	async register(data: UserDTO): Promise<UserRO> {
-		const { username } = data;
-		let user = await this.repository.findOne({ username });
-		if (user) {
-			throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+		const toUpdate = await this.findOneById(id);
+		if (!(await bcrypt.compare(password, toUpdate.password))) {
+			password = await bcrypt.hash(password, 10);
+			data.password = password;
 		}
-		user = await this.repository.create(data);
-		await this.repository.save(user);
-		return user.toResponseObject();
+		const updated = Object.assign(toUpdate, data);
+		const entity = await this.userRepository.save(updated);
+		return entity;
 	}
 
-	private get repository(): Repository<User> {
-		return this.userRepository;
+	async delete(id: string): Promise<User> {
+		return super.delete(id);
 	}
 }
