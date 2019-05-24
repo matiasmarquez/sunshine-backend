08630fc9314998ae5314b559101df21f1a6eb57b
matiasmdev <main.mmarquez@gmail.com>
2019-05-23 22:38:23 -0300

    Removed all relationated with token and RO. Added properties name and lastName.

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -4,11 +4,10 @@ import {
 	Column,
 	CreateDateColumn,
 	BeforeInsert,
+	BeforeUpdate,
 } from 'typeorm';
 
 import * as bcrypt from 'bcryptjs';
-import * as jwt from 'jsonwebtoken';
-import { UserRO } from './user.dto';
 
 @Entity('users')
 export class User {
@@ -21,6 +20,12 @@ export class User {
 	})
 	username: string;
 
+	@Column({ type: 'varchar' })
+	name: string;
+
+	@Column({ type: 'varchar' })
+	lastName: string;
+
 	@Column('varchar')
 	password: string;
 
@@ -30,30 +35,5 @@ export class User {
 	@BeforeInsert()
 	async hashPassword() {
 		this.password = await bcrypt.hash(this.password, 10);
-	}
-
-	toResponseObject(showToken: boolean = true): UserRO {
-		const { id, created, username, token } = this;
-		const responseObject: UserRO = { id, created, username };
-		if (showToken) {
-			responseObject.token = token;
-		}
-		return responseObject;
-	}
-
-	async comparePassword(attempt: string) {
-		return await bcrypt.compare(attempt, this.password);
-	}
-
-	private get token() {
-		const { id, username } = this;
-		return jwt.sign(
-			{
-				id,
-				username,
-			},
-			process.env.SECRET,
-			{ expiresIn: '7d' },
-		);
 	}
 }
